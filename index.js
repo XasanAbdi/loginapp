477b97aef9d633c9ce652915d54c7d46ab2a8d1b
XasanAbdi <xasancabdinasax@gmail.com>
2023-04-06 23:35:19 -0700

    test

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ dotenv.config()
 const app =express();
 const port=3000;
 app.use(express.json());
-app.get('/',(req,res)=>{
-    res.send('welcome to node js and expresss')
-})
+
 
 app.get('/',async(req, res)=>{
 
